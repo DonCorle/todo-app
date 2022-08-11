35a2b064635e5d0a5f19ff2d746d6936b083f268
Your Name <you@example.com>
2022-08-11 17:13:42 +0900

    첫번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Lists from "./components/Lists";
 
-function App() {
+export default function App() {
+  //첫번째 인수 : 변수 이름, 두번째 인수 : State를 정하는 함수
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow-xl lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+        </div>
+        <Lists todoData={todoData} setTodoData={setTodoData} />
+        <Form value={value} handleSubmit={handleSubmit} setValue={setValue} />
+      </div>
     </div>
   );
 }
-
-export default App;
